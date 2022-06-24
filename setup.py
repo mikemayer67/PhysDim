5a31a36a16cd4d72596c7f47cbc157b9e905761a
Michael A Mayer <mikemayer67@vmwishes.com>
2022-06-24 11:01:17 -0400

    restructure for setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='physdim',
+    version='0.1.0',
+    packages=['physdim'],
+    url='https://github.com/mikemayer67/physdim',
+    license='unlicense',
+    author='Michael Mayer',
+    author_email='mikemayer67@vmwishes.com',
+    description='Physical Dimensionality',
+    install_reqires=['numpy'],
+)
